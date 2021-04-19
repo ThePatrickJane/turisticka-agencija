22c1a9c4f360221a080fdcb7c1db00e6bb2601ed
Patrick Jane <mikicamiki.bat@gmail.com>
2021-04-19 11:23:55 +0200

    drugi

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <v-app-bar color="blue darken-2" dark height="70">
+    <v-app-bar color="blue darken-3" dark height="80" flat>
       <v-toolbar-title class="text-uppercase">
         <span class="text-h4">Turisticka</span>
         <span style="color: lightblue" class="">agencija</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <Register class="hidden-xs-only" />
-      <Login class="hidden-xs-only" />
-      <v-app-bar-nav-icon class="hidden-sm-and-up" @click="drawer = true"></v-app-bar-nav-icon>
+      <Register class="hidden-sm-and-down" />
+      <Login class="hidden-sm-and-down" />
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" absolute temporary color="blue darken-2" dark>
-      <v-row class="pt-5">
+    <v-navigation-drawer v-model="drawer" absolute temporary color="blue darken-4" dark>
+      <!-- <v-row class="pt-5">
         <v-col cols="12">
           <Login />
         </v-col>
@@ -20,6 +20,48 @@
       <v-row>
         <v-col cols="12">
           <Register />
+        </v-col>
+      </v-row> -->
+      <v-row class="pt-5">
+        <v-col cols="12">
+          <v-card
+            color="transparent"
+            class="mx-auto"
+            tile
+            flat
+          >
+            <v-list flat nav>
+              <v-subheader class="hidden-md-and-up">Prijava i Registracija</v-subheader>
+              <v-list-item-group color="white">
+                <v-list-item class="hidden-md-and-up">
+                  <v-list-item-icon>
+                    <v-icon>mdi-login-variant</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <Login />
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item class="hidden-md-and-up">
+                  <v-list-item-icon>
+                    <v-icon>mdi-account-plus</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <Register />
+                  </v-list-item-content>
+                </v-list-item>
+                <v-divider class="hidden-md-and-up"></v-divider>
+                <v-subheader>Korisnici</v-subheader>
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>mdi-account</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-btn text>Korisnici</v-btn>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-card>
         </v-col>
       </v-row>
     </v-navigation-drawer>
